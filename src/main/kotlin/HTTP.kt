a6d2cdbb74cd8505d86b774ec74f255634d26ff3
Karl J. Overå <k@rl.run>
2025-08-21 12:01:54 +0200

    chore: support x-request-id

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,10 +1,16 @@
 package no.nav.tsm
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.openapi.*
 import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
+    install(CallId) {
+        retrieveFromHeader(HttpHeaders.XRequestId)
+    }
+
     routing {
         openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
     }
